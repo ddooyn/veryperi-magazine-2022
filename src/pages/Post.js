1c50fc1427d9ff2bdbbf0b7b9a175a22659983f9
Sooyoung Yoon <ayoooo240@gmail.com>
2022-07-21 05:41:18 +0900

    ✨ Post 작성 페이지 마크업 및 사진 첨부 기능 추가

--- src/pages/Post.js
+++ src/pages/Post.js
@@ -1,11 +1,225 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { useForm } from 'react-hook-form';
 
 const Post = () => {
+  const {
+    handleSubmit,
+    formState: { isValid },
+  } = useForm({
+    mode: 'all',
+  });
+
+  const [imgName, setImgName] = useState('');
+  const [imgSrc, setImgSrc] = useState('');
+
+  const previewImage = async (e) => {
+    const image = e.target.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(image);
+    return new Promise((resolve) => {
+      reader.onload = () => {
+        setImgName(image.name);
+        setImgSrc(reader.result);
+        resolve();
+      };
+    });
+  };
+
+  const onSubmit = async (data) => {
+    return;
+  };
+
   return (
-    <div>
-      <h2>Post</h2>
-    </div>
+    <PostSection>
+      <h2>
+        포스트<span>작성</span>
+      </h2>
+
+      <PostForm onSubmit={handleSubmit(onSubmit)}>
+        <Row className="row-img">
+          <input
+            id="file"
+            type="file"
+            accept="image/*"
+            onChange={previewImage}
+            hidden
+          />
+          <ImgNameLabel>
+            <ImgNameInput
+              type="text"
+              placeholder="사진을 첨부해주세요"
+              value={imgName}
+              readOnly
+            />
+          </ImgNameLabel>
+          <ImgAddBtn htmlFor="file">
+            <label htmlFor="file">사진 선택</label>
+          </ImgAddBtn>
+        </Row>
+
+        <Row>
+          <h3>레이아웃 선택</h3>
+          <Radios>
+            <div>
+              <input type="radio" name="layout" id="layout-a" defaultChecked />
+              <label htmlFor="layout-a">사진/글(좌우)</label>
+            </div>
+            <div>
+              <input type="radio" name="layout" id="layout-b" />
+              <label htmlFor="layout-b">글/사진(좌우)</label>
+            </div>
+            <div>
+              <input type="radio" name="layout" id="layout-c" />
+              <label htmlFor="layout-c">글/사진(상하)</label>
+            </div>
+          </Radios>
+        </Row>
+
+        <Preview>
+          <ImgLabel>{imgSrc && <img src={imgSrc} alt="" />}</ImgLabel>
+          <p>텍스트</p>
+        </Preview>
+
+        <Row>
+          <h3>포스트 내용</h3>
+          <PostTextArea
+            // ref={ref}
+            // onChange={onChange}
+            // onInput={resizeHeight}
+            // value={typed}
+            placeholder="포스트 내용을 입력하세요 :)"
+            spellCheck="false"
+          />
+        </Row>
+      </PostForm>
+      <Button type="submit" disabled={!isValid}>
+        게시글 작성하기
+      </Button>
+    </PostSection>
   );
 };
 
 export default Post;
+const PostSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  h2 {
+    margin-bottom: 10px;
+    span {
+      color: #6768ab;
+    }
+  }
+`;
+
+const PostForm = styled.form`
+  width: 80vw;
+  max-width: 760px;
+  margin-bottom: 30px;
+`;
+
+const Row = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-wrap: wrap;
+  margin: 30px 0 15px;
+  &.row-img {
+    margin-bottom: 30px;
+    border-bottom: 1px solid #555;
+  }
+  label {
+    display: inline-block;
+    width: 100px;
+    text-align: center;
+  }
+  input {
+    padding: 10px 0;
+    border: none;
+    outline: none;
+    font-size: 16px;
+    font-style: italic;
+  }
+`;
+
+const ImgNameLabel = styled.label`
+  flex: 1;
+`;
+
+const ImgNameInput = styled.input`
+  width: 100%;
+  color: #aaa;
+`;
+
+const ImgAddBtn = styled.button`
+  margin-left: 5px;
+  background: #222;
+  color: #fff;
+  label {
+    width: 100%;
+    height: 100%;
+    padding: 11px 20px;
+    cursor: pointer;
+  }
+`;
+
+const ImgLabel = styled.label`
+  display: block;
+  overflow: hidden;
+  width: 335px;
+  height: 245px;
+  border: 1px solid #aaa;
+  border-radius: px;
+  background: #f4f4f4 url(/img/noimage.png) center;
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+`;
+
+const Radios = styled.div`
+  display: flex;
+  gap: 15px;
+  div {
+    text-align: center;
+  }
+`;
+
+const Preview = styled.div`
+  display: flex;
+  /* flex-direction: column-reverse; */
+  justify-content: space-around;
+  align-items: center;
+  p {
+    width: 335px;
+    text-align: center;
+  }
+`;
+
+const PostTextArea = styled.textarea`
+  width: 100%;
+  margin-top: 12px;
+  padding: 30px;
+  border: 1px solid #aaa;
+  background: none;
+  font-size: 18px;
+  outline: none;
+  resize: none;
+  &::placeholder {
+    color: #aaa;
+  }
+`;
+
+const Button = styled.button`
+  padding: 20px;
+  background: #222;
+  color: #fff;
+  font-size: 16px;
+  &:disabled {
+    background: #ccc;
+    cursor: default;
+  }
+`;
